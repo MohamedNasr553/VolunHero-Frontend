b9935cf4b9570cb54068ad13c6351aea199e47fa
Mahmoud Nader Ali <91666764+mahmoudnader150@users.noreply.github.com>
2024-02-08 01:36:54 +0200

    Medical Help Page

--- lib/src/app_root.dart
+++ lib/src/app_root.dart
@@ -5,6 +5,7 @@ import 'package:flutter_code/modules/EditProfile/editProfile_Page.dart';
 import 'package:flutter_code/modules/ForgetPassword/ForgetPassword_Page.dart';
 import 'package:flutter_code/modules/GetSupport/Support_Page.dart';
 import 'package:flutter_code/modules/Login/Login_Page.dart';
+import 'package:flutter_code/modules/MedicalHelp/MedicalHelp_Page.dart';
 import 'package:flutter_code/modules/Notifications/Notifications_Page.dart';
 import 'package:flutter_code/modules/OnBoarding/OnBoarding_Page.dart';
 import 'package:flutter_code/modules/ResetPassword/ResetPassword_Page.dart';
@@ -19,7 +20,7 @@ class AppRoot extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: lightTheme,
-      home: Questions(),
+      home: MedicalHelp(),
     );
   }
 }
